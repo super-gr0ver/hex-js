e9379cad5cf4cf7626bea42b9bc6ab5baeed9a1f
super-gr0ver_srv <e.dorodnov@gmail.com>
2022-07-29 20:42:48 +0000

    fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import _ from "lodash";
-console.log("Hello, Hexlet!");
+import _ from 'lodash';
 
-console.log(_.last(["one", "two"]));
-
+console.log('Hello, Hexlet!');
+console.log(_.last(['one', 'two']));
 const fn = (a, b) => {
   const c = a + b;
   return c;
 };
+console.log(fn(1, 2));
